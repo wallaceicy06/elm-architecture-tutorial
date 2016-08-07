6ac91512136358a72f2e23bbb87b113752188a79
Sean Harger <seanharger93@gmail.com>
2016-08-06 23:48:42 -0700

    Implemented more form validators, cleaned up error display.
    Added validators on the password to ensure that a password is
    * longer than 8 characters
    * contain uppercase, lowercase, and numerical characters
    
    Added an age field and a validator that checks that it is an integer.
    
    Added a submit button that only displays validation errors when clicked.

--- examples/3-form.elm
+++ examples/3-form.elm
@@ -1,11 +1,12 @@
 import Html exposing (..)
-import Html.App as Html
+import Html.App as App
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-
+import Html.Events exposing (onInput, onClick)
+import String
+import Char
 
 main =
-  Html.beginnerProgram
+  App.beginnerProgram
     { model = model
     , view = view
     , update = update
@@ -18,14 +19,16 @@ main =
 
 type alias Model =
   { name : String
+  , age: String
   , password : String
   , passwordAgain : String
+  , hideErrors: Bool
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" "" True
 
 
 
@@ -34,21 +37,29 @@ model =
 
 type Msg
     = Name String
+    | Age String
     | Password String
     | PasswordAgain String
+    | ShowErrors
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Name name ->
-      { model | name = name }
+      { model | hideErrors = True, name = name }
+      
+    Age age ->
+      { model | hideErrors = True, age = age }
 
     Password password ->
-      { model | password = password }
+      { model | hideErrors = True, password = password }
 
     PasswordAgain password ->
-      { model | passwordAgain = password }
+      { model | hideErrors = True, passwordAgain = password }
+      
+    ShowErrors ->
+      { model | hideErrors = False }
 
 
 
@@ -59,8 +70,10 @@ view : Model -> Html Msg
 view model =
   div []
     [ input [ type' "text", placeholder "Name", onInput Name ] []
+    , input [ type' "text", placeholder "Age", onInput Age] []
     , input [ type' "password", placeholder "Password", onInput Password ] []
     , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , button [ onClick ShowErrors ] [ text "Submit" ]
     , viewValidation model
     ]
 
@@ -69,9 +82,26 @@ viewValidation : Model -> Html msg
 viewValidation model =
   let
     (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
+      if String.all Char.isDigit model.age /= True then
+        ("red", "Age must be an integer")
+      else if model.password /= model.passwordAgain then
         ("red", "Passwords do not match!")
+      else if String.length model.password <= 8 then
+        ("red", "Password is too short")
+      else if containsAllRequiredCharacters model.password /= True then
+        ("red", "The password must contain mixed case with at least one number")
+      else
+        ("green", "OK")
   in
-    div [ style [("color", color)] ] [ text message ]
+    div [ style [("color", color), ("visibility", hideErrorsVisibility model.hideErrors)] ] [ text message ]
+
+hideErrorsVisibility hideErrorsValue =
+  if hideErrorsValue == True then
+    "hidden"
+  else
+    "visible"
+
+containsAllRequiredCharacters password =
+  String.any Char.isDigit password &&
+  String.any Char.isUpper password &&
+  String.any Char.isLower password
